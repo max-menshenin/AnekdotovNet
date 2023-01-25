2c8a6a68b976534fad5139bdfe952090ec774b99
Postmind <53375202+max-menshenin@users.noreply.github.com>
2023-01-25 13:59:25 +0300

    upd

--- Parser.py
+++ Parser.py
@@ -1,11 +1,12 @@
 from bs4 import BeautifulSoup as BS
 import requests as r
 r = r.get('http://anekdotov.net/')
-r.encoding= 'windows-1251'
+r.encoding = 'windows-1251'
 
 soup = BS(r.text, 'lxml')
 i = 0
-for item in soup.find_all('div'):
-    if i<=5 :
-        print(item.text)
+for item in soup.find_all('div', {"class": 'anekdot'}):
+    if i <= 5:
+        print(item.text, end='\n')
         i = i-1
+# mydivs = soup.find_all("div", {"class": "stylelistrow"})
